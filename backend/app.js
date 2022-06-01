686684017ceca29fd99dd768d4e4f2acd70cbd96
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-06-01 18:42:43 +0530

    connected mongo

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,15 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 app.use("/", (req, res, next) => {
   res.send("Hello hai bhai ko");
 });
-app.listen(5000);
+
+mongoose
+  .connect(
+    "mongodb+srv://mohitchauhan4u:9306138730@blogwebsitemern.wlhbo.mongodb.net/test"
+  )
+  .then(() => app.listen(5000))
+  .then(() => console.log("Connected To Database"))
+  .catch((error) => console.log("ERROR", console.error));
