9d33a8cd6ad7b2d7be54bf76a28aa7718134e3ba
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-06-02 17:26:11 +0530

    created login functionality

--- backend/controllers/user-controller.js
+++ backend/controllers/user-controller.js
@@ -42,3 +42,23 @@ export const signup = async (req, res, next) => {
   }
   return res.status(201).json({ user });
 };
+
+export const login = async (req, res, next) => {
+  const { email, password } = req.body;
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email });
+  } catch (error) {
+    return console.error("ERROR", error);
+  }
+  if (!existingUser) {
+    return res
+      .status(404)
+      .json({ message: "Could not find any user by this email " });
+  }
+  const isPasswordCorrect = bcrypt.compareSync(password, existingUser.password);
+  if (!isPasswordCorrect) {
+    return res.status(400).json({ message: "Incorrect Password" });
+  }
+  return res.status(200).json({ message: "Login Succesfull!!" });
+};
