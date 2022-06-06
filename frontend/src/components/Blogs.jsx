80d48ff02884d142a87cef01e1f7cbca5ab808cc
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-06-06 21:35:15 +0530

    added get all blog functionality

--- frontend/src/components/Blogs.jsx
+++ frontend/src/components/Blogs.jsx
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 const Blogs = () => {
+  const [blogs, setBlogs] = useState();
+  const sendRequest = async () => {
+    const res = await axios
+      .get("http://localhost:5000/api/blog")
+      .catch((error) => console.error("ERROR", error));
+    const data = await res.data;
+    return data;
+  };
+  useEffect(() => {
+    sendRequest().then((data) => setBlogs(data.blogs));
+  }, []);
+  console.log("BLOGS", blogs);
   return <div>Blogs</div>;
 };
 
