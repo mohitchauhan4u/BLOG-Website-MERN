39ffc32da224ae09b211e507889f95bb81234b86
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-06-12 15:36:08 +0530

    added refreah login to redux

--- frontend/src/components/UserBlog.jsx
+++ frontend/src/components/UserBlog.jsx
@@ -1,33 +1,34 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Blog from "./Blog";
-
 const UserBlog = () => {
   const [user, setUser] = useState();
   const id = localStorage.getItem("userId");
   const sendRequest = async () => {
     const res = await axios
       .get(`http://localhost:5000/api/blog/user/${id}`)
-      .catch((error) => console.error("ERROR", error));
+      .catch((err) => console.log(err));
     const data = await res.data;
     return data;
   };
   useEffect(() => {
     sendRequest().then((data) => setUser(data.user));
-  });
-  console.log("USERBLOGS", user);
+  }, []);
+  console.log(user);
   return (
     <div>
+      {" "}
       {user &&
         user.blogs &&
-        user.blogs.map((blog, key) => (
+        user.blogs.map((blog, index) => (
           <Blog
+            id={blog._id}
+            key={index}
             isUser={true}
-            key={key}
             title={blog.title}
             description={blog.description}
-            image={blog.image}
-            user={user.name}
+            imageURL={blog.image}
+            userName={user.name}
           />
         ))}
     </div>
