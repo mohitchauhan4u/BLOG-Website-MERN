31e643fb0ce809194c95dd60d1c24338dbbab700
mohitchauhan4u <mohitchauhan1929@gmail.com>
2022-06-12 15:29:43 +0530

    added roboto text

--- frontend/src/components/utils.jsx
+++ frontend/src/components/utils.jsx
@@ -1,7 +1,6 @@
 import { makeStyles } from "@mui/styles";
-
-const useStyles = makeStyles({
+export const useStyles = makeStyles({
   font: {
-    fontFamily: "Roboto",
+    fontFamily: "Roboto !important",
   },
 });
